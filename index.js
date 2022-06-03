d462ef2407a99015c42fb6677af9081f0c9a330d
NaveenDalal92 <naveen.dalal92@gmail.com>
2022-06-03 09:54:25 +0530

    board-server

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+app.use(cors());
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    method: ["GET", "POST"],
+  },
+});
+
+let dbData = {
+  open: [],
+  inProcess: [],
+  completed: [],
+};
+
+io.on("connection", (socket) => {
+  console.log(`${socket.id} connected`);
+
+  socket.on("join", (data) => {
+    socket.join(1);
+    console.log("user joined", dbData);
+    socket.emit("update", dbData);
+  });
+  socket.on("send", (data) => {
+    dbData = { ...data };
+    console.log("received", dbData);
+    io.in(1).emit("update", dbData);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`${socket.id} disconnected`);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("Server Running");
+});
